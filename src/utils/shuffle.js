669a049fa274ff6974c80c96bf30204530afaa8c
Max McDaniel <maxm@piestar.com>
2020-10-28 17:30:35 -0500

    Extract shuffle function

---
+++ src/utils/shuffle.js
@@ -0,0 +1,17 @@
+/**
+ * "Randomly" shuffle an array. https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+ * @param {Array} arr
+ * @return {Array}
+ */
+module.exports = function (arr) {
+    let j, temp
+
+    for (let i = arr.length; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1))
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+    }
+
+    return arr
+}
