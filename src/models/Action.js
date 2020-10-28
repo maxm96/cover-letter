e2c072a1b2cda0b3436eddacafebbdd34a961085
Max McDaniel <maxm@piestar.com>
2020-10-28 12:41:22 -0500

    Remove Action.js

--- src/models/Action.js
+++
@@ -1,9 +0,0 @@
-class Action
-{
-    constructor(player1, card1, player2 = null, card2 = null) {
-        this.player1 = player1
-        this.card1 = card1
-        this.player2 = player2
-        this.card2 = card2
-    }
-}
