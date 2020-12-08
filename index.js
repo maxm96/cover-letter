74546b85c479ace3f8b255efaafac62eca7e0f73
Max McDaniel <maxm@piestar.com>
2020-12-08 17:02:21 -0600

    Use heroku's port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const redis = require('redis')
 const session = require('express-session')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 app.use(bodyParser.json())
