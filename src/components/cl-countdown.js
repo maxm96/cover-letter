3deec12aa5bb991348e646319156a53667974db2
Max McDaniel <maxm@piestar.com>
2021-02-11 09:54:54 -0600

    Add countdown component

---
+++ src/components/cl-countdown.js
@@ -0,0 +1,76 @@
+import Component from './component'
+
+class ClCountdown extends Component
+{
+    constructor() {
+        super()
+
+        const shadow = this.attShad()
+        const container = this.createContainer()
+
+        this.time = this.getAttr('time')
+        this.countdownHandle = null
+
+        container.classList.add('countdown')
+        container.innerHTML = this.template
+
+        this.countdownEl = this.shadowRoot.querySelector('.countdown')
+
+        this.setTime = this.setTime.bind(this)
+        this.startCountdown = this.startCountdown.bind(this)
+        this.stopCountdown = this.stopCountdown.bind(this)
+
+        shadow.appendChild(container)
+    }
+
+    get template() {
+        return `
+        <style>
+        .countdown {
+            text-align: center;
+            margin-top: 15px;
+            font-size: 1.8em;
+        }
+        
+        .time {
+            font-size: 26px;
+            padding: 10px;
+        }
+        </style>
+        
+        <span class="time">${this.time}</span>
+        `
+    }
+
+    setTime(time) {
+        this.time = time
+        this.countdownEl.innerText = time
+    }
+
+    startCountdown() {
+        if (this.countdownHandle !== null)
+            return
+
+        this.countdownHandle = setInterval(() => {
+            if (--this.time > -1) {
+                this.countdownEl.innerText = this.time
+            } else {
+                this.stopCountdown()
+
+                // Emit an event to let parent know that countdown has finished
+                this.dispatchEvent(new Event('countdownfinished'))
+            }
+        }, 1000)
+    }
+
+    stopCountdown() {
+        clearInterval(this.countdownHandle)
+        this.countdownHandle = null
+    }
+
+    connectedCallback() {
+        this.setTime(this.getAttr('time'))
+    }
+}
+
+customElements.define('cl-countdown', ClCountdown)
